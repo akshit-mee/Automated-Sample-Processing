865746eff9f2310ae92004d222a954a2e6e0bdbc
akshit.gupta <akshit.gupta@rwth-aachen.de>
2024-12-28 04:36:29 +0100

    feat: update ExperimentSettingForm to use IntegerField for numeric inputs and add validation

--- display/forms.py
+++ display/forms.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, IntegerField, SubmitField
+from wtforms.validators import DataRequired, NumberRange, Optional
 
 class ExperimentSettingForm(FlaskForm):
     experiment_name = StringField('Experiment Name', validators=[DataRequired()])
     person_responsible = StringField('Person Responsible', validators=[DataRequired()])
     experiment_description = TextAreaField('Experiment Description', validators=[DataRequired()])
-    number_of_samples = StringField('Number of Samples', validators=[DataRequired()])
-    thermomixer_time_s = StringField('Thermomixer Time (s)', validators=[DataRequired()])
-    liquid_nitrogen_time_s = StringField('Liquid Nitrogen Time (s)', validators=[DataRequired()])
-    number_of_cycles = StringField('Number of Cycles', validators=[DataRequired()])
-    additional_notes = TextAreaField('Additional Notes', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    number_of_samples = IntegerField('Number of Samples', validators=[DataRequired(), NumberRange(min=1)])
+    thermomixer_time_s = IntegerField('Thermomixer Time (s)', validators=[DataRequired(), NumberRange(min=1)])
+    liquid_nitrogen_time_s = IntegerField('Liquid Nitrogen Time (s)', validators=[DataRequired(), NumberRange(min=1)])
+    number_of_cycles = IntegerField('Number of Cycles', validators=[DataRequired(), NumberRange(min=1)])
+    additional_notes = TextAreaField('Additional Notes', validators=[Optional()])
+    submit = SubmitField('Save Settings')
