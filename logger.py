cf243536fff99cc406bda5b59f0841780de18571
Gupta, Akshit <gupta@dwi.rwth-aachen.de>
2025-05-20 16:55:41 +0200

    changed logger to log path is absoloute

--- logger.py
+++ logger.py
@@ -4,13 +4,13 @@ from datetime import datetime
 import os
 import time
 
-def setup_logger(log_dir = 'logs'):
+def setup_logger(log_dir = '/home/er/Desktop/Code/logs'):
     os.makedirs(log_dir, exist_ok = True)
     
     timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    log_file = os.path.join(log_dir, f'app_{timestamp}.log')
+    log_file = os.path.join(log_dir, f'asp_{timestamp}.log')
     
-    logger = logging.getLogger('my_app')
+    logger = logging.getLogger('ASP')
     logger.setLevel(logging.DEBUG)
     
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
