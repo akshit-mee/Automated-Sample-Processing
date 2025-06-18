7f3335e62404241f5ffd7eac7f476d649038c321
akshit.gupta <akshit.gupta@rwth-aachen.de>
2025-06-18 18:07:27 +0200

    Refactors codebase into modular configuration and utilities
    Separates hardcoded coordinates and robot settings into dedicated config module for better maintainability and reusability.
    
    Extracts common utility functions including logging setup, distance calculations, temperature reading, and closest point determination into standalone utils module.
    
    Adds temperature monitoring functionality to water bath operations for improved process tracking and debugging capabilities.

--- robot_actions.py
+++ robot_actions.py
@@ -1,32 +1,20 @@
-from pymycobot.mycobot import MyCobot
 import time
 import threading
 import requests
 import RPi.GPIO as GPIO
 import numpy
-import logger
+import config
+import utils
 
 setting_flag = 0
-log = logger.setup_logger()
+log = utils.setup_logger()
 log.info("Starting Robot Actions")
 
 class StopCycleException(Exception):
     pass
 
-robot_actions = ['Innitial Setting Updated',
-                 'Wainting', 
-                 'Place Sample in Thermomixer', 
-                 'Place Sample in Liquid Nitrogen', 
-                 'Pick up Sample from Thermomixer', 
-                 'Pick up Sample from Liquid Nitrogen',
-                 'Moving Sample to Thermomixer', 
-                 'Moving Sample to Liquid Nitrogen'
-                 'Completed'
-                 'Stopped by User'
-                 'Restarted by User']
-
 mc = MyCobot("/dev/ttyAMA0", 1000000)
-cobot_speed = 100
+cobot_speed = config.speed
 
 
 
@@ -154,23 +142,20 @@ class RobotActions:
     def __init__(self, mc):
         self.thermomixer = [0,0,0]   #add coordinates of thermomixer
         self.liquid_nitrogen = [0,0,0] #add coordinates of liquid nitrogen
-        self.current_cycle = 1
-        self.number_of_cycles = 1
-        self.thermomixer_time = 1
-        self.liquid_nitrogen_time = 1
-        self.waiting_time = 1
+        self.current_cycle = None
+        self.number_of_cycles = None
+        self.thermomixer_time = None
+        self.liquid_nitrogen_time = None
+        self.waiting_time = None
 
-        self.c1 = [-18.0, -207.0, 210.0, -175.0, -5.0, -144.08]             #LN2 (Fully sumbersed is [181.0, -161.8, 180, 176.08, 0, 45.13])
-        self.c2 = [-2.7, -206.6, 253.6, -175.6, -5.0, -144.88]            #Above LN2
-        self.a2 = [-70.92, -27.24, -19.59, -41.39, -6.5, 45.35]
-        
-        self.c3 = [205.2, -16.0, 260, 178.65, -6.81, -64.68]
-        self.c4 = [205.2, -16.0, 215, 180.0, 0.0, -64.68]
-        self.a4 = [15.11, -11.42, -71.1, -3.51, -5.71, -10.81]
-
-        self.cm = [103.0, -24.2, 286.2, 175.16, -2.62, -20.54]
-        self.cr = [104.0, 187.1, 245.3, -175.69, -0.98, -52.58]   
-        # self.cm2 = [134.4, -1.3, 298.7, 170.73, -8.85, 55.61]        #Above Water-Bath
+        self.c1 = config.c1
+        self.c2 = config.c2
+        self.a2 = config.a2
+        self.c3 = config.c3
+        self.c4 = config.c4
+        self.a4 = config.a4
+        self.cm = config.cm
+        self.cr = config.cr
 
         
     def update_settings(self, settings):
@@ -186,13 +171,6 @@ class RobotActions:
             log.info(f"Waiting time: {self.waiting_time}")
             log.info(f"Number of cycles: {self.number_of_cycles}")
 
-    ################################ Basic Coordinate Distance ##########################
-    def distance(self, point1, point2):
-        sum = 0
-        for i in range(3):
-            sum = sum + (point1[i] - point2[i])**2
-        distance = numpy.sqrt(sum)
-        return distance   
 
     ############################ check loction error ###################################
 
@@ -211,7 +189,7 @@ class RobotActions:
                 time.sleep(0.1)
         
         if position_value_flag:
-            distance_error = self.distance(expected_position, current_position)
+            distance_error = utils.distance(expected_position, current_position)
             
             if distance_error <= 10:
                 #print(f'\033[92m{current_position = }: \033[0m')
@@ -240,13 +218,6 @@ class RobotActions:
             time.sleep(2)
         while(not self.is_correct_position(coordinate)):
             mc.send_coords(coordinate, speed, mode)
-
-
-    def sample_position(self):
-        pass
-
-    def wait(self, time):
-        pass
     
         
     def _check_staus(self):
@@ -276,20 +247,6 @@ class RobotActions:
         time.sleep(0.5)
         mc.release_all_servos()
         log.info ("Completed and relesed motors")
-        
-    def closest_point(self, position = mc.get_coords()):
-        d1 = self.distance(position, self.c2)
-        d2 = self.distance(position, self.c3)
-        d3 = self.distance(position, self.cr)
-    
-        if d1 <= d2 and d1 <= d3:
-            return self.c2
-        
-        if d2 <= d3 and d2 <= d1:
-            return self.c3
-    
-        if d3 <= d1 and d3 <= d1:
-            return self.cr
     
     def manual_stop(self, timeout = 5):
         
@@ -308,7 +265,7 @@ class RobotActions:
         #print(time.monotonic() - start_time)
         
         if position_value_flag:
-            close_point = self.closest_point(current_position)
+            close_point = utils.closest_point(current_position)
             log.info(f"Currently at {current_position}")
             log.info(f"Moving to {close_point}")
             self.move(close_point, cobot_speed, 1, mc, False)
@@ -351,12 +308,14 @@ class RobotActions:
             update_robot_log("Moving sample to Water Bath", self.current_cycle, gripper_state, mc.get_error_information())
             self.move(self.c3)
             log.info("Moving Inside Water Bath")
+            log.info(f"Water Temperature = {utils.read_temp()}")
             start_water_bath_time = time.monotonic()
             update_robot_log("Moving Inside Water Bath", self.current_cycle, gripper_state, mc.get_error_information())
             self.move(self.c4)
             self.delay(self.thermomixer_time -2)
             end_water_bath_time = time.monotonic()
             water_bath_time = start_water_bath_time - end_water_bath_time
+            log.info(f"Water Temperature = {utils.read_temp()}")
             log.info("Moving outside Water Bath")
             # print(f"Water Bath time = {water_bath_time}")
             update_robot_log("Moving outside Water Bath", self.current_cycle, gripper_state, mc.get_error_information())
@@ -377,18 +336,15 @@ class RobotActions:
         
     ################################################################################
 
-
-    def complete(self):
-        pass
-
-    def stop(self):
-        pass
-
         
 if __name__ == "__main__":
     
     ra = RobotActions(mc)
     settings_old = None
+
+    mc.release_all_servos()
+    time.sleep(0.5)
+    mc.release_all_servos()
     
     while True:
         controll = get_robot_control()
---
+++ utils.py
@@ -0,0 +1,91 @@
+import logging
+from logging.handlers import RotatingFileHandler
+from datetime import datetime
+import os
+import time
+import numpy
+import glob
+
+import config
+
+
+
+################################ Logging Functionality ##########################
+def setup_logger(log_dir = '/home/er/Desktop/Code/logs'):
+    os.makedirs(log_dir, exist_ok = True)
+    
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    log_file = os.path.join(log_dir, f'asp_{timestamp}.log')
+    
+    logger = logging.getLogger('ASP')
+    logger.setLevel(logging.DEBUG)
+    
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    
+    file_handler = RotatingFileHandler(
+        log_file,
+        maxBytes = 1024 * 1024, #1 mb
+        backupCount = 1,
+        encoding = 'utf-8',
+    )
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    
+    return logger
+
+
+
+################################ Basic Coordinate Distance ##########################
+def distance(self, point1, point2):
+    sum = 0
+    for i in range(3):
+        sum = sum + (point1[i] - point2[i])**2
+    distance = numpy.sqrt(sum)
+    return distance
+
+################################ Temperature Sensor Functionality ##########################
+### One Wire ##################
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+    
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':       #check if it is a valid reading
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equal_pos = lines[1].find('t=')
+    if equal_pos != -1:
+        temp_strings = lines[1][equal_pos+2:]
+        temp_c = float(temp_strings)/1000.0
+        return temp_c
+
+
+################################ Closest Point Functionality ##########################
+def closest_point(self, position):
+    d1 = self.distance(position, config.c2)
+    d2 = self.distance(position, config.c3)
+    d3 = self.distance(position, config.cr)
+
+    if d1 <= d2 and d1 <= d3:
+        return config.c2
+
+    if d2 <= d3 and d2 <= d1:
+        return config.c3
+
+    if d3 <= d1 and d3 <= d2:
+        return config.cr
